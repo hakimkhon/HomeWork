feabc7dd1739ac3b0996a4ba03e2e318af1d20b9
Hakimkhon <hakimkhon94@gmail.com>
2023-02-15 21:47:05 +0500

    coding bad

---
+++ src/mobile/New.java
@@ -0,0 +1,18 @@
+package mobile;
+
+public class New {
+    public static void main(String[] args) {
+        System.out.println(("xsadaxxa"));
+
+    }
+    public static int countX(String str){
+
+        return 1;
+    }
+
+    public static String removeLastChar(String s) {
+        return (s == null || s.length() == 0)
+                ? null
+                : (s.substring(0, s.length() - 1));
+    }
+}
